29647307ab16167a8984691837f554938d85588a
Nadya Jackson <nadya.jackson@bryanuniversity.edu>
2022-02-11 15:32:00 -0600

    heroku updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: '../.env' });
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const expressJwt = require('express-jwt');
+const path = require('path')
 
 app.use(express.json()); 
 app.use(morgan('dev')) ;
@@ -11,7 +12,7 @@ app.use(morgan('dev')) ;
 main().catch(err => console.log(err))
 async function main(){
 //Conect DB mongodb+srv://voter-1:Kibeth13!@cluster0.pvl01.mongodb.net/RockTheVote
-mongoose.connect('mongodb+srv://voter-1:Kibeth13!@cluster0.pvl01.mongodb.net/RockTheVote')
+mongoose.connect( process.env.MONGODB_URI || 'mongodb+srv://voter-1:Kibeth13!@cluster0.pvl01.mongodb.net/RockTheVote')
   console.log("Connected to the DB")
 }
 app.use('/api', expressJwt({secret: process.env.SECRET, algorithms: ['HS256']}));
@@ -29,6 +30,17 @@ app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 });
 
-app.listen(9000, () => { 
-    console.log("The App is listening on port 9000")
+const PORT = process.env.PORT || 9000;
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('voiting/build'));
+  app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'voting', 'build', 'index.html')); //relative path
+  })
+}
+
+
+
+app.listen(PORT, () => { 
+    console.log(`The App is listening on port ${PORT}`)
 });
